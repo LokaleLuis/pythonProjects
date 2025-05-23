d93d41b826599911a38a9ae4529c8da2d75eae70
LokaleLuis <L.maerkens@icloud.com>
2025-05-23 16:27:58 +0200

    Create main.py

---
+++ #04 password generator/main.py
@@ -0,0 +1,9 @@
+def generate_password(length=12):
+    import random
+    import string
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+print(generate_password())
