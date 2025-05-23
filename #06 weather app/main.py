fd21d35e772ffeb736f65a65846157f6a4216355
LokaleLuis <L.maerkens@icloud.com>
2025-05-23 17:59:08 +0200

    2

---
+++ #06 weather app/main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title("Weather App")
+window.geometry("300x200")
+
+    
+test = tk.Label(window, text="Hello, World!")
+test.pack()
+
+window.mainloop()
